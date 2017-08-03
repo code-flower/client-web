5cc11d44d50714e488d1ec39e71629df262129bb
Jake Mensch <jake.mensch@gmail.com>
2017-08-03 10:45:31 -0700

    fullscreen button working

--- src/js/app/directives/appContainer.js
+++ src/js/app/directives/appContainer.js
@@ -14,6 +14,7 @@ angular.module('CodeFlower')
 
   function link (scope, el, attrs) {
     scope.state = state;
+    scope.isFullscreen = false;
 
     scope.openModal = function() {
       $uibModal.open({
@@ -21,6 +22,24 @@ angular.module('CodeFlower')
         templateUrl: PARTIALS_DIR + 'modals/main-modal.html',
         animation: false
       });
+    };
+
+    scope.toggleFullscreen = function() {
+      var app = document.getElementsByClassName('app-container')[0];
+
+      if (!scope.isFullscreen) {
+        if (app.mozRequestFullScreen)
+          app.mozRequestFullScreen();
+        else
+          app.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+        scope.isFullscreen = true;
+      } else {
+        if (document.mozCancelFullScreen)
+          document.mozCancelFullScreen();
+        else
+          document.webkitExitFullscreen();
+        scope.isFullscreen = false;
+      }
     };
 
     // for testing modal
